24842ed595d4c3922848c52b348064d9cda8019c
James Dunn <james.ed.dunn@gmail.com>
2017-10-25 15:55:00 +0900

    datetime only search wip

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,15 +2,34 @@ class EventsController < ApplicationController
 
   def index
      @events = Event.all
-    if params[:search_loc]
+    if params[:search_loc] != "" && params[:search_date] != ""
+      @location = params[:search_loc]
+      @datetime = Date.strptime(params[:search_date], "%m/%d/%Y")
+      @unsorted_events = Event.by_distance(:origin => "#{@location}")
+      @events = []
+      for event in @unsorted_events
+        if event.datetime > @datetime
+          @events << event
+        end
+      end
+    elsif params[:search_loc] != ""
       @location = params[:search_loc]
       @events = Event.by_distance(:origin => "#{@location}")
+    elsif params[:search_date] != ""
+      @datetime = Date.strptime(params[:search_date], "%m/%d/%Y")
+      @events = []
+      Event.all.each do |event|
+        if event.datetime > @datetime
+          @events << event
+        end
+      end
     else
       @events = Event.all.order("created_at DESC")
     end
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
