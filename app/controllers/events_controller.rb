730afdfb8950b705c7aa78023e16eb991dc44321
James Dunn <james.ed.dunn@gmail.com>
2017-10-25 13:08:27 +0900

    event search by location

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,37 @@
 class EventsController < ApplicationController
+
+  def index
+     @events = Event.all
+    if params[:search_loc]
+      @location = params[:search_loc]
+      @events = Event.by_distance(:origin => "#{@location}")
+    else
+      @events = Event.all.order("created_at DESC")
+    end
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def event_params
+
+  end
+
 end
