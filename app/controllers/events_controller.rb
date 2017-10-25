969282e547eb659e4615552303ddf30870ffeb2b
James Dunn <james.ed.dunn@gmail.com>
2017-10-25 17:36:32 +0900

    game search added - full functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,20 @@ class EventsController < ApplicationController
 
   def index
      @events = Event.all
-    if params[:search_loc] != "" && params[:search_date] != ""
+
+    if params[:search_loc] != "" && params[:search_date] != "" && params[:search_game] != ""
+      @location = params[:search_loc]
+      @game = Game.where('name ILIKE ?', "%#{params[:search_game]}%")
+      @datetime = Date.strptime(params[:search_date], "%m/%d/%Y")
+      @unsorted_events = Event.by_distance(:origin => "#{@location}")
+      @events = []
+      for event in @unsorted_events
+        if event.datetime > @datetime && @game.include?(event.game)
+          @events << event
+        end
+      end
+
+    elsif params[:search_loc] != "" && params[:search_date] != ""
       @location = params[:search_loc]
       @datetime = Date.strptime(params[:search_date], "%m/%d/%Y")
       @unsorted_events = Event.by_distance(:origin => "#{@location}")
@@ -12,9 +25,35 @@ class EventsController < ApplicationController
           @events << event
         end
       end
+
+    elsif params[:search_loc] != "" && params[:search_game] != ""
+      @location = params[:search_loc]
+      @game = Game.where('name ILIKE ?', "%#{params[:search_game]}%")
+      @unsorted_events = Event.by_distance(:origin => "#{@location}")
+      @events = []
+      for event in @unsorted_events
+        if @game.include?(event.game)
+          @events << event
+        end
+      end
+
+    elsif params[:search_date] != "" && params[:search_game] != ""
+      @game = Game.where('name ILIKE ?', "%#{params[:search_game]}%")
+      @datetime = Date.strptime(params[:search_date], "%m/%d/%Y")
+      @unsorted_events = Event.where(game: @game)
+      @events = []
+      for event in @unsorted_events
+       if event.datetime > @datetime
+          @events << event
+        end
+      end
+
+
     elsif params[:search_loc] != ""
       @location = params[:search_loc]
       @events = Event.by_distance(:origin => "#{@location}")
+
+
     elsif params[:search_date] != ""
       @datetime = Date.strptime(params[:search_date], "%m/%d/%Y")
       @events = []
@@ -23,9 +62,15 @@ class EventsController < ApplicationController
           @events << event
         end
       end
+
+    elsif params[:search_game] != ""
+      @game = Game.where('name ILIKE ?', "%#{params[:search_game]}%")
+      @events = Event.where(game: @game)
+
     else
       @events = Event.all.order("created_at DESC")
     end
+
   end
 
   def show
