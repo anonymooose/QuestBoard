3d04d2d4a3272820d15a7c35a7cbbe0e0a9136d8
James Dunn <james.ed.dunn@gmail.com>
2017-10-25 09:45:48 +0900

    usernames saving - removed overwriting accessor atrribute in model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :username, :description)
+    params.require(:user).permit(:email, :username, :description, :login)
   end
 
 end
