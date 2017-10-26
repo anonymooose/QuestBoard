0b312bc5854247235513a7627e92b582b30e20d4
James Dunn <james.ed.dunn@gmail.com>
2017-10-26 16:38:30 +0900

    event creation form

--- app/assets/javascripts/google_maps_autocomplete.js
+++ app/assets/javascripts/google_maps_autocomplete.js
@@ -1,6 +1,4 @@
 
-
-
 var address = document.getElementById('search_loc');
 var autocomplete = new google.maps.places.Autocomplete(address, { types: ['geocode'] });
 google.maps.event.addDomListener(address, 'keydown', function(e) {
