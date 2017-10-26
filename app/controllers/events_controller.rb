9f234445ee3741749515523f513de43a514247ff
James Dunn <james.ed.dunn@gmail.com>
2017-10-26 11:19:31 +0900

    event controller methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -78,24 +78,48 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.owner = current_user
+    if @event.save
+      flash[:notice] = "Event successfully listed!"
+      redirect_to event_path(@event)
+    else
+      flash[:alert] = "Invalid information."
+      render :new
+    end
   end
 
   def edit
+    @event = Event.find(params[:id])
+    if @event.owner != current_user
+      flash[:alert] = "You don't own this event."
+      redirect_to root_path
+    end
   end
 
   def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :edit
+    end
   end
 
   def delete
+    @event = Event.find(params[:id])
+    @event.destroy
   end
 
   private
 
   def event_params
-
+    params.require(:event).permit(:title, :description, :game_id, :address, :datetime)
   end
 
 end
