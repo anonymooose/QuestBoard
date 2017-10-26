9a0e097d2f55be153f18cfee5995620608250e12
Rose Fernandez <kareru.rose@gmail.com>
2017-10-26 14:28:24 +0900

    Homepage styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
 
   def home
     @games = Game.all
-    @events = Event.all.order(created_at: :desc).first(6)
+    @events = Event.all.order(created_at: :desc).first(3)
   end
 end
