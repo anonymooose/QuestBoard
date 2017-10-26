de18f99bcd0e526fec15cd921b38d9a5b8d108e5
Trevor Malard <redsky.dz@gmail.com>
2017-10-25 20:25:07 -0700

    Implement logic for setting Survey vote, NEED AUTO EVENT WINNER

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,15 +1,22 @@
+
 class Survey < ApplicationRecord
   belongs_to :player
   has_one :event, through: :player
-  after_update :disallow_editing
+  before_update :disallow_editing
+  after_update :set_winner_if_needed
 
   def vote
     self.vote_id.nil? ? nil : Player.find(self.vote_id)
   end
 
   def vote=(player)
-    self.vote_id = player.id
-    valid_player?(player) ? self.save : false
+    if player == 0
+      self.destroy
+    else
+      self.vote_id = player.id
+      valid_player?(player) ? self.save : false
+      disallow_editing
+    end
   end
 
   private
@@ -19,5 +26,11 @@ class Survey < ApplicationRecord
 
   def disallow_editing
     self.attended = true
+  end
+
+  def set_winner_if_needed
+    #event = self.event
+    votes = self.event.surveys.where.not(vote_id:nil)
+    count = votes.pluck(:vote_id).group_by(&:itself).max_by{|_,v|v.size}.first
   end
 end
