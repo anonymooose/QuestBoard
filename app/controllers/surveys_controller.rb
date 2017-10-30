695c2660f720c9de1f2aba09845b21834e36a9eb
Trevor Malard <redsky.dz@gmail.com>
2017-10-30 00:39:37 -0700

    logic for checking level up in place, need achievement grant logic

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -18,7 +18,13 @@ class SurveysController < ApplicationController
     attendance = survey_params[:attended] == 'true'
     attendance ? @survey.vote = Player.find(survey_params[:vote].to_i) : @survey.vote = 0
     if attendance
-
+      if level_up?(current_user.level)
+        add_rewards
+        if new_achievements?(current_user.achievements)
+        end
+      else
+        add_reward
+      end
       flash[:notice] = "Thanks for answering! Here's 5 coins!"
     else
       flash[:notice] = "Thanks for being honest! Here's 1 coin!"
@@ -31,6 +37,17 @@ class SurveysController < ApplicationController
     params.require(:survey).permit(:vote, :attended)
   end
 
-  def check_for_achievements
+  def new_achievements?(achieve_array)
+  end
+
+  def level_up?(initial_level)
+    new_level = initial_level + (@survey.event.experience)/100
+    return true if initial_level.floor < new_level.floor
+    return false
+  end
+
+  def add_rewards
+    @survey.user.level += @survey.event.experience
+    @survey.user.coins += @survey.event.coins
   end
 end
