b76713a12b9596e84d112ac4846c8a81001d4afb
Carla Almazora <carla.almazora@gmail.com>
2017-10-30 16:17:21 +0900

    Add avatar stylesheet, avatar javascript, add images, fixed html for avatar edit

---
+++ app/assets/javascripts/avatar.js
@@ -0,0 +1,45 @@
+const avatar = () => {
+  $('.avatar').each((i, el) => {
+    $(el).html('');
+
+    // $(el).append(`<img src="/assets/avatar/gender/${$(el).data('gender')}.svg">`);
+
+    // if ($(el).data('hair') > 0) {
+    //   $(el).append(`<img src="/assets/avatar/hair/${$(el).data('hair')}.svg">`);
+    // }
+
+    // if ($(el).data('top') > 0) {
+    //   $(el).append(`<img src="/assets/avatar/top/${$(el).data('top')}.svg">`);
+    // }
+
+    // ...
+    // $(el).append(`<img src="/assets/avatar/bottom/${$(el).data('bottom')}.svg">`);
+    // $(el).append(`<img src="/assets/avatar/shoes/${$(el).data('shoes')}.svg">`);
+
+    const avatar = {
+      gender: $(el).data('gender'),
+      hair: $(el).data('hair'),
+      top: $(el).data('top'),
+      bottom: $(el).data('bottom'),
+      shoes: $(el).data('shoes'),
+    };
+    console.log(avatar)
+    for (let type in avatar) {
+      if (type === 'gender' || avatar[type] > 0) {
+        $(el).append(`<img src="/assets/avatar/${type}/${avatar[type]}.svg">`);
+      }
+    }
+  });
+}
+
+avatar();
+
+
+
+$('.avatar-choice input').change(e => {
+  // "avatar[hair]" => "hair"
+  const type = $(e.currentTarget).attr('name').split('[')[1].slice(0, -1);
+  const value = parseInt($(e.currentTarget).val());
+  $('.avatar').data(type, value);
+  avatar();
+});
