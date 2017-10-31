a5c89c00d84a055d1cd8efe19f4a1cc13586adbe
Trevor Malard <redsky.dz@gmail.com>
2017-10-31 03:45:40 -0700

    more event survey refresh checks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  before_action :user_surveys
 
   def home
     @games = Game.all
     @events = Event.all.order(created_at: :desc).first(3)
+  end
+
+  private
+  def user_surveys
+    current_user.surveys! unless current_user.nil?
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user.surveys!
     @events = Player.where(user_id:@user.id)
   end
 
