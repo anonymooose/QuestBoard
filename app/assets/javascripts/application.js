00b3f4aaa3a24e27d3cb20c3462873c2b4bbe700
James Dunn <james.ed.dunn@gmail.com>
2017-10-31 12:12:02 +0900

    toast notifications replace flashes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,27 @@
 //= require jquery-ui/widgets/datepicker
 //= require jquery-ui/widgets/autocomplete
 //= require autocomplete-rails
+//= require toastr
 //= require moment
 //= require_tree .
 
 console.log("hello from js")
+
+/*global toastr*/
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-bottom-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
