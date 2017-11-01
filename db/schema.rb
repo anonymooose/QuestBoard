6accef06c8959f9658a6b81c2e1543228bbfb7b4
trevor malard <redsky.dz@gmail.com>
2017-11-01 19:52:17 +0900

    event page datetime strftime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030072552) do
+ActiveRecord::Schema.define(version: 20171101095826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20171030072552) do
     t.float    "lng"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "citycountry"
     t.index ["game_id"], name: "index_events_on_game_id", using: :btree
     t.index ["host_id"], name: "index_events_on_host_id", using: :btree
     t.index ["win_id"], name: "index_events_on_win_id", using: :btree
