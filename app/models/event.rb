4e4ba06eeb6a4316ac2be85019599cfcd895321c
James Dunn <james.ed.dunn@gmail.com>
2017-11-01 14:02:30 +0900

    Revert "Datepicker"

--- app/models/event.rb
+++ app/models/event.rb
@@ -45,12 +45,13 @@ class Event < ApplicationRecord
     self.datetime < Time.now ? true : false
   end
 
-  def time=(val)
-    @time = val
-  end
-
-  def time
-    @time
+  def ultra_delete!
+    #when a user truly can no longer host the event, and players must be notified
+    #there cannot be any surveys
+    if self.past?
+      #self.players.each do |player| {player.cancelled << Cancel.new(event:self.title)}
+      return true
+    end
   end
 
   private
