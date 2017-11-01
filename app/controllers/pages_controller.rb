ae9e85224673e23b7890e974222ff5f0e9c2a2d6
James Dunn <james.ed.dunn@gmail.com>
2017-11-01 14:56:36 +0900

    Revert "Homepage events"

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
 
   def home
     @games = Game.all
-    @events = Event.where('datetime > ?', Time.now).order(created_at: :desc).first(3)
+    @events = Event.all.order(created_at: :desc).first(3)
   end
 
   private
